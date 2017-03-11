bbb2d28f37cd3e363e3b739ecf129b064f3331d8
kennedyhightechcursos <kennedy@hightechcursos.com.br>
2017-03-11 08:48:50 -0400

    Projeto inicializado

---
+++ src/main/java/com/htcursos/TesteDeSoftwarePersistenciaApplication.java
@@ -0,0 +1,12 @@
+package com.htcursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeSoftwarePersistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDeSoftwarePersistenciaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/htcursos/TesteDeSoftwarePersistenciaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.htcursos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteDeSoftwarePersistenciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
